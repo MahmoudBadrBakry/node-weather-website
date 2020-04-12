c46ed7796d54d62bc1fab037386af4494df797a8
Mahmoud Badr <mbadr1210@gmail.com>
2020-04-13 00:10:08 +0200

    adding low and high temperature

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -3,8 +3,9 @@ const request = require('request')
 //    https://api.darksky.net/forecast/31fa890fa99c7689cb34bea9f206c872/31.23944,30.05611?units=si
 const forecast = (longitude,latitude,callback)=>{
     let url = "https://api.darksky.net/forecast/31fa890fa99c7689cb34bea9f206c872/"+latitude+","+longitude+"?units=si"
+    //console.log(url)
     request({url , json:true},(connectionError,{body})=>{
-        const {error,currently,daily} = body
+        const {error,currently,daily,hourly} = body
         if(connectionError){
             callback("error in getting data from network")
         }else if(error){
@@ -12,7 +13,11 @@ const forecast = (longitude,latitude,callback)=>{
         }else {
             let temperature = currently.temperature
             let precipProbability = currently.precipProbability*100.0
-            callback(undefined,daily.data[0].summary + ' it\'s currently ' + temperature + ' degrees out. There is a ' + precipProbability + '% chance of Rain')
+            callback(undefined, 'hi :)   the weather is ' + daily.data[0].summary + ' the highest temprature is '
+            + hourly.data[17].temperature
+            + '    and the lowwest is ' + hourly.data[5].temperature 
+            + '    it\'s currently ' + temperature 
+            + ' degrees out. There is a ' + precipProbability + '% chance of Rain')
         }
     })
 }
